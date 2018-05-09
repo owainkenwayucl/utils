2a213b8edf7c0a87a11ec5ddbc3f02e4bd0fea86
Owain Kenway <o.kenway@ucl.ac.uk>
2018-05-09 13:07:12 +0100

    Add ssh-decorate to the list of dubious modules.
    Backdoored.

--- src/pipscan.sh
+++ src/pipscan.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for a in acqusition apidev-coop bzip crypt django-server pwd setup-tools telnet urlib3 urllib
+for a in acqusition apidev-coop bzip crypt django-server pwd setup-tools telnet urlib3 urllib ssh-decorate
 do
   $1 list $2 | grep --color=always $a
 done
