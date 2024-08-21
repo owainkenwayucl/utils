214d1e483fb4a4b4a70be6e514bef4fa27bfde43
Owain Kenway <o.kenway@ucl.ac.uk>
2024-08-21 11:42:08 +0100

    Easy wallpaper shell scripts

---
+++ src/centred-wallpaper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# turn on centred wallpaper in gnome from the command-line
+
+gsettings set org.gnome.desktop.background picture-options 'centered'
---
+++ src/span-wallpaper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# turn on spanned wallpaper in gnome from the command-line
+
+gsettings set org.gnome.desktop.background picture-options 'spanned'
